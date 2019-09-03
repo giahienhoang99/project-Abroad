40a40e042eab31f64862a53ffd6dad86dcf2dc9e
giahienhoang99 <giahienhoang99@gmail.com>
2019-09-03 18:07:28 +0700

    project

---
+++ 2.web.js
@@ -0,0 +1,14 @@
+document.getElementById("USAweb").onclick = function openUSA() {
+    window.open("4.US.html",'_blank');
+}
+document.getElementById("CANweb").onclick = function openCanada() {
+    window.open("5.Can.html",'_blank');
+}
+document.getElementById("UKweb").onclick = function openUK() {
+    window.open("6.UK.html",'_blank');
+}
+function openHomePage() {
+    location.href = "1.web.html";
+}
+ 
+ 
